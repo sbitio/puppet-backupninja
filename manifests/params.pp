29f3e466b6ba849635fae7edaa93054dae8b718a
Pedro González Serrano <niteman.es@gmail.com>
2014-08-22 17:25:23 +0200

    centos/redhat support take 1

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,20 +14,22 @@ class backupninja::params (
   $backupdir       = '/var/backups',
 ) {
 
+  $package_name     = 'backupninja'
+  $config_file      = '/etc/backupninja.conf'
+  $config_dir       = '/etc/backup.d'
+  $logfile          = '/var/log/backupninja.log'
+  $admingroup       = 'root'
+  $scriptdirectory  = '/usr/share/backupninja'
+
   case $::operatingsystem {
     ubuntu, debian: {
-      $package_name     = 'backupninja'
-      $config_file      = '/etc/backupninja.conf'
-      $config_dir       = '/etc/backup.d'
-      $logfile          = '/var/log/backupninja.log'
-      $admingroup       = 'root'
-      $scriptdirectory  = '/usr/share/backupninja'
       $libdirectory     = '/usr/lib/backupninja'
       $mysql_configfile = '/etc/mysql/debian.cnf'
     }
-#    redhat, centos: {
-#      $package_name    = 'backupninja'
-#    }
+    redhat, centos: {
+      $libdirectory     = '/usr/libexec/backupninja'
+      $mysql_configfile = undef
+    }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
     }
