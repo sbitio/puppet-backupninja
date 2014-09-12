24b6722870247499e7442b4971e42c41253cb459
Pedro González Serrano <niteman.es@gmail.com>
2014-09-12 21:01:48 +0200

    added suport for extra handlers autodeploy

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,11 @@
 class backupninja (
-  $ensure      = present,
-  $autoupgrade = true,
+  $ensure         = present,
+  $autoupgrade    = true,
+  $extra_handlers = [],
 ) {
+
+  validate_bool($autoupgrade)
+  validate_array($extra_handlers)
 
   case $ensure {
     /(present)/: {
@@ -26,4 +30,14 @@ class backupninja (
   include backupninja::install
   include backupninja::config
 
+  if $extra_handlers != [] {
+    backupninja::load_extra_handler { $extra_handlers :}
+  }
+}
+
+define backupninja::load_extra_handler () {
+  backupninja::handler { $name :
+    handler_source => "puppet:///modules/backupninja/handlers/${name}.in",
+    helper_source  => "puppet:///modules/backupninja/handlers/${name}.helper.in",
+  }
 }
