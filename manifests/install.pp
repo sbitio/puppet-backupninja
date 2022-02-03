0dc082efd81377d9fc71a4722f357755800f6699
kris <kris@sbit.io>
2022-02-03 18:29:48 +0100

    fix indentation of => is not properly aligned for install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,7 +3,7 @@ class backupninja::install () {
   require backupninja::params
 
   package { $backupninja::params::package_name:
-    ensure  => $backupninja::package_ensure,
+    ensure          => $backupninja::package_ensure,
     install_options => $::backupninja::params::package_install_options,
   }
 
