d6b40eecc6bf8f6aef4ad9f2430f8854e9379085
kris <kris@sbit.io>
2022-02-04 12:16:58 +0100

    Fix puppet-lint: backupninja::load_extra_handler not in autoload module layout by creating a singular file for the defined type

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,15 +38,3 @@ class backupninja (
     backupninja::load_extra_handler { $extra_handlers :}
   }
 }
-
-# backupninja::load_extra_handlers
-#
-# This defined type is responsible for loading the extra backupninja handlers.
-# It calls another class backupninja::handler, and passes it the necessary parameters to load the handlers
-#
-define backupninja::load_extra_handler () {
-  backupninja::handler { $name :
-    handler_source => "puppet:///modules/backupninja/handlers/${name}.in",
-    helper_source  => "puppet:///modules/backupninja/handlers/${name}.helper.in",
-  }
-}
